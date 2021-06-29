6210880f061087af8335db7db66086caf9e077c2
villa942 <villajuanmora@gmail.com>
2021-06-28 22:38:05 -0500

    dashboard, crear ente funcional falta mensaje de exito

---
+++ Plantilla/controlador/Controlador.php
@@ -0,0 +1,26 @@
+<?php
+
+require '../conexion.php';
+require '../dao/enteDao.php';
+require '../dto/enteDto.php';
+
+if(isset($_POST['registroe'])){
+
+    $uDao = new EnteDao();
+    $uDto = new EnteDto();
+    $uDto->setNit($_POST['nit']);
+    $uDto->setRazon($_POST['razon']);
+    $uDto->setRepresentante($_POST['representante']);
+    $uDto->setCorreo($_POST['correo']);
+    $uDto->setSitio($_POST['sitio']);
+    $uDto->setCiudad($_POST['ciudad']);
+    $uDto->setCapacidad($_POST['capacidad']);
+
+
+    $mensaje = $uDao->registrarEnte($uDto);
+
+    header("Location:../ente.php?mensaje=".$mensaje);
+}
+
+
+?>
